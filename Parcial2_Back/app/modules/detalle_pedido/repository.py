ceda853ec7894ed2841877c2d6d9d3a9854e1e38
PabloAVivas <pablovivas.mdz@gmail.com>
2026-05-19 17:27:35 -0300

    Delete Parcial2_Back directory

--- Parcial2_Back/app/modules/detalle_pedido/repository.py
+++
@@ -1,7 +0,0 @@
-from sqlmodel import Session, select, func, delete
-from app.core.repository import BaseRepository
-from app.modules.detalle_pedido.models import DetallePedido
-
-class DetallePedidoRepository(BaseRepository[DetallePedido]):
-    def __init__(self, session: Session) -> None:
-        super().__init__(session, DetallePedido)
--- Parcial2_Back/app/modules/pedido/repository.py
+++
@@ -1,7 +0,0 @@
-from sqlmodel import Session, select, func, delete
-from app.core.repository import BaseRepository
-from app.modules.pedido.models import Pedido
-
-class PedidoRepository(BaseRepository[Pedido]):
-    def __init__(self, session: Session) -> None:
-        super().__init__(session, Pedido)
