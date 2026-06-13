228929bd22027494f909d421209328a7d66a66c6
PabloAVivas <pablovivas.mdz@gmail.com>
2026-06-13 01:43:14 -0300

    arreglo en config y la tabla pago

--- app/modules/pago/models.py
+++ app/modules/pago/models.py
@@ -27,7 +27,7 @@ class Pago(SQLModel, table=True):
     external_reference: str = Field(sa_column=Column(String(100),unique=True, nullable=False))
     idempotency_key: str = Field(sa_column=Column(String(100),unique=True, nullable=False))
     transaction_amount: float = Field(max_digits=10, decimal_places=2, nullable=False)
-    payment_method_id: str = Field(sa_column=Column(String(50)), nullable=True, default= None)
+    payment_method_id: str = Field(default= None, sa_column=Column(String(50), nullable=True))
 
     created_at: datetime = Field(sa_column=Column(DateTime(timezone=True), nullable=False, default=lambda: datetime.now(timezone.utc)))
     updated_at: datetime = Field(sa_column=Column(DateTime(timezone=True), nullable=False, default=lambda: datetime.now(timezone.utc), onupdate=lambda: datetime.now(timezone.utc)))
