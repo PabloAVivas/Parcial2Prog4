f4983b38f112f9a8e507427ee48cb0494ad67531
PabloAVivas <pablovivas.mdz@gmail.com>
2026-06-16 01:05:41 -0300

    MercadoPago confirmar_pago corregido

--- app/modules/pago/services.py
+++ app/modules/pago/services.py
@@ -5,7 +5,9 @@ from fastapi import HTTPException, status
 from sqlmodel import Session
 from app.core.config import settings
 from app.modules.pago.models import Pago
-from app.modules.pedido.models import Pedido
+from app.modules.producto.models import Producto
+from app.modules.ingrediente.models import Ingrediente
+from app.modules.pedido.models import Pedido, HistorialEstadoPedido
 from app.modules.pago.schemas import PagoCrearResponse, PagoEstadoResponse
 from app.modules.pago.unit_of_work import PagoUnitOfWork
 from datetime import datetime, timezone
@@ -147,6 +149,51 @@ class PagoService:
                 public_key=self._get_mp_public_key(),
             )
 
+    def _get_producto_or_404(self, uow:PagoUnitOfWork, producto_id: int) -> Producto:
+        producto = uow.producto.get_by_id_categorias_ingredientes(producto_id)
+        if not producto:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail=f"Producto con el id {producto_id} no encontrado",
+            )
+        return producto
+    
+    def _get_ingrediente_or_404(self, uow:PagoUnitOfWork, ingrediente_id: int) -> Ingrediente:
+        ingrediente = uow.ingrediente.get_by_id(ingrediente_id)
+        if not ingrediente:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail=f"Ingrediente con el id {ingrediente_id} no encontrado",
+            )
+        return ingrediente
+
+    def _actualizar_pedido_or_404(self, uow:PagoUnitOfWork, pedido_id: int, nuevo_estado: str):
+        pedido = uow.pedido.get_by_id_pedido(pedido_id)
+        if not pedido:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail=f"Pedido con id={pedido_id} no encontrado",
+            )
+        if pedido.estado_codigo == "PENDIENTE":
+            if nuevo_estado == "CANCELADO":
+                for depe in pedido.detalle_pedidos:
+                    producto = self._get_producto_or_404(uow, depe.producto_id)
+                    producto.stock_cantidad += depe.cantidad
+                    for ingrediente_producto in producto.ingrediente_links:
+                        if ingrediente_producto.ingrediente_id not in depe.personalizacion:
+                            ingrediente = self._get_ingrediente_or_404(uow, ingrediente_producto.ingrediente_id)
+                            cantidad_sumar = ingrediente_producto.cantidad * depe.cantidad
+                            ingrediente.stock_cantidad += cantidad_sumar
+            historial = HistorialEstadoPedido(
+                    pedido_id= pedido.id,
+                    estado_desde= pedido.estado_codigo,
+                    estado_hasta= nuevo_estado
+                )
+            uow.historial_estado_pedido.add(historial)
+
+            pedido.estado_codigo = nuevo_estado
+            uow.pedido.add(pedido)
+
     def procesar_webhook(self, data: dict, query_params: Optional[dict] = None) -> dict:
         logger.info("Webhook recibido: data=%s qs=%s", data, query_params or {})
         if not data and query_params:
@@ -194,13 +241,12 @@ class PagoService:
                 pago.mp_status_detail = mp_info.get("mp_status_detail")
                 pago.payment_method_id = mp_info.get("payment_method_id")
                 uow.pago.add(pago)
-                pedido = uow.pedido.get_by_id(pago.pedido_id)
                 if nuevo_estado == "aprobado":
-                    pedido.estado_codigo = "CONFIRMADO"
-                    uow.pedido.add(pedido)
+                    nuevo_estado = "CONFIRMADO"
+                    self._actualizar_pedido_or_404(uow, pago.pedido_id, nuevo_estado)
                 elif nuevo_estado == "rechazado":
-                    pedido.estado_codigo = "CANCELADO"
-                    uow.pedido.add(pedido)
+                    nuevo_estado = "CANCELADO"
+                    self._actualizar_pedido_or_404(uow, pago.pedido_id, nuevo_estado)
 
             return {
                 "status": "processed",
@@ -222,33 +268,34 @@ class PagoService:
             )
 
         resolved_payment_id = payment_id
-        if not resolved_payment_id:
-            with PagoUnitOfWork(self._session) as uow:
+        with PagoUnitOfWork(self._session) as uow:
+            if not resolved_payment_id:
                 pago_local = uow.pago.get_ultimo_by_pedido(pedido_id)
                 if pago_local and pago_local.mp_payment_id:
                     resolved_payment_id = pago_local.mp_payment_id
 
-        if resolved_payment_id:
-            try:
-                mp_info = self._consultar_pago_mp(resolved_payment_id)
-            except RuntimeError as e:
-                raise HTTPException(
-                    status_code=status.HTTP_400_BAD_REQUEST,
-                    detail=str(e),
-                )
-
-            estado_mp = mp_info.get("mp_status")
-            if estado_mp == "approved":
-                nuevo_estado = "aprobado"
-            elif estado_mp in ("rejected", "cancelled", "refunded", "charged_back"):
-                nuevo_estado = "rechazado"
-            else:
-                nuevo_estado = "pendiente"
-
-            with PagoUnitOfWork(self._session) as uow:
+            if resolved_payment_id:
+                try:
+                    mp_info = self._consultar_pago_mp(resolved_payment_id)
+                except RuntimeError as e:
+                    raise HTTPException(
+                        status_code=status.HTTP_400_BAD_REQUEST,
+                        detail=str(e),
+                    )
+                estado_mp = mp_info.get("mp_status")
                 pago = uow.pago.get_by_mp_payment_id(resolved_payment_id)
                 if not pago:
                     pago = uow.pago.get_ultimo_by_pedido(pedido_id)
+                if not pago:
+                    return PagoEstadoResponse(estado="ignored, reason: Pago not found in local DB", pedido_id=pedido_id)
+                if pago.mp_status is not None and pago.mp_status not in ("pending", "in_process", "authorized"):
+                        return PagoEstadoResponse(estado=pago.mp_status, pedido_id=pedido_id)
+                if estado_mp == "approved":
+                    nuevo_estado = "aprobado"
+                elif estado_mp in ("rejected", "cancelled", "refunded", "charged_back"):
+                    nuevo_estado = "rechazado"
+                else:
+                    nuevo_estado = "pendiente"
 
                 if pago:
                     pago.mp_payment_id = resolved_payment_id
@@ -258,18 +305,18 @@ class PagoService:
                     uow.pago.add(pago)
 
                     if nuevo_estado == "aprobado":
-                        pedido.estado_codigo = "CONFIRMADO"
-                        uow.pedido.add(pedido)
+                        nuevo_estado = "CONFIRMADO"
+                        self._actualizar_pedido_or_404(uow, pago.pedido_id, nuevo_estado)
                     elif nuevo_estado == "rechazado":
-                        pedido.estado_codigo = "CANCELADO"
-                        uow.pedido.add(pedido)
+                        nuevo_estado = "CANCELADO"
+                        self._actualizar_pedido_or_404(uow, pago.pedido_id, nuevo_estado)
 
-            return PagoEstadoResponse(estado=nuevo_estado, pedido_id=pedido_id)
+                return PagoEstadoResponse(estado=nuevo_estado, pedido_id=pedido_id)
 
 
-        with PagoUnitOfWork(self._session) as uow:
-            pago_local = uow.pago.get_ultimo_by_pedido(pedido_id)
-            return PagoEstadoResponse(
-                estado=pago_local.mp_status if pago_local else None,
-                pedido_id=pedido_id,
-            )
+            else:
+                pago_local = uow.pago.get_ultimo_by_pedido(pedido_id)
+                return PagoEstadoResponse(
+                    estado=pago_local.mp_status if pago_local else None,
+                    pedido_id=pedido_id,
+                )
